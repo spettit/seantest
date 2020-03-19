7ed60ac996fb0829f226589093e3f56e167ba68d
Sean Pettit <s.p@mac.com>
2020-03-19 15:29:04 +0000

    add identity widget plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     'gatsby-transformer-json',
+    'gatsby-plugin-netlify-identity-widget',
     {
       resolve: `gatsby-plugin-typography`,
       options: {
